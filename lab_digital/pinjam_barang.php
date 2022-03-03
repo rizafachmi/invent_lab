60146301a06d8d67c97cb0e9d9b1ef8a88df89cd
Rahman <abdurrahman1270@gmail.com>
2022-03-03 16:11:01 +0700

    add fitur pinjam

---
+++ lab_digital/pinjam_barang.php
@@ -0,0 +1,13 @@
+<?php
+    include_once '../config.php';
+    $id_alat = $_GET['id_alat'];
+
+    $query = "UPDATE t_alat_digital SET status_pinjam='Sudah Dipinjam' WHERE id_alat='$id_alat'";
+    
+    $exec = mysqli_query($koneksi, $query);
+    if ($exec){
+        echo "<script> alert ('data berhasil dihapus!'); document.location='index.php'</script>";
+    }else{
+        echo "<script> alert ('data gagal dihapus!'); document.location='index.php'</script>";
+    }
+    // mysqli_close($koneksi);
